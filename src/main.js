3d5a95048bd94ff218432be3598aadc5d2a9a1fd
LX <1392846225@qq.com>
2021-05-09 21:29:01 +0800

    chore: change

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import Antd from 'ant-design-vue'
+// import 'ant-design-vue/dist/antd.css'
 import App from './App.vue'
 
+// Vue.use(Antd)
+
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
